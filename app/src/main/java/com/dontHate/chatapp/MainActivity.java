cc1898433c1782c2c114dbd0c94d5f193faf9bf1
Sandeep Undrakonda <sandeepundarakonda1234@gmail.com>
2022-09-09 16:14:56 +0530

    had some issues with makermen rounded corners also work on SignUpActivity

--- app/src/main/java/com/dontHate/chatapp/MainActivity.java
+++ app/src/main/java/com/dontHate/chatapp/MainActivity.java
@@ -1,4 +1,12 @@
 package com.dontHate.chatapp;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.nohate.databinding.ActivitySignUpBinding;
 
 public class MainActivity extends android.app.Activity {
+
+
+
+    
 }
--- app/src/main/java/com/dontHate/chatapp/activities/SignUpActivity.java
+++ app/src/main/java/com/dontHate/chatapp/activities/SignUpActivity.java
@@ -4,13 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.nohate.R;
+
+import com.example.nohate.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+
     }
+    private void setListeners(){
+        binding.textSignIn.setOnClickListener(v ->
+                onBackPressed());
+    }
+
 }
