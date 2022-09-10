e5909245ed851b36f26b383476ca778fae1a4f2a
Sandeep Undrakonda <sandeepundarakonda1234@gmail.com>
2022-09-10 16:06:02 +0530

    FireBase and Database setup done

--- app/src/main/java/com/dontHate/chatapp/activities/SignInActivity.java
+++ app/src/main/java/com/dontHate/chatapp/activities/SignInActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.nohate.R;
 import com.example.nohate.databinding.ActivitySignInBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -21,5 +25,7 @@ public class SignInActivity extends AppCompatActivity {
     private void setListeners(){
         binding.textCreateNewAccount.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
+
     }
+
 }
